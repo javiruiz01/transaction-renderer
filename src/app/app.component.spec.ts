a897faa54722537394f808ad003db8bee5f20577
Javier Ruiz Calle <jarc0212@gmail.com>
2018-06-22 08:24:14 +0200

    Add placeholder to table in case of error

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -3,6 +3,7 @@ import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 import { AppComponent } from "./app.component";
 import { SearchButtonComponent } from "./search-button/search-button.component";
 import { SelectOptionsComponent } from "./select-options/select-options.component";
+import { TransactionService } from "./transaction-service.service";
 import { TransactionsTableComponent } from "./transactions-table/transactions-table.component";
 
 describe("AppComponent", () => {
@@ -17,7 +18,7 @@ describe("AppComponent", () => {
         SearchButtonComponent,
         TransactionsTableComponent
       ],
-      providers: [HttpClient, HttpHandler]
+      providers: [HttpClient, HttpHandler, TransactionService]
     }).compileComponents();
   }));
 
@@ -26,22 +27,4 @@ describe("AppComponent", () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-
-  /* it(`should have 'transactionOptions'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.transactionOptions).toEqual([
-      { value: "payment", viewValue: "Payment" },
-      { value: "credit", viewValue: "Credit" }
-    ]);
-  }));
-
-  it("should render title in a h1 tag", async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector("h1").textContent).toContain(
-      "Welcome to transaction-renderer!"
-    );
-  })); */
 });
